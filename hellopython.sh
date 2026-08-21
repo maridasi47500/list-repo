3267c28ec22b8c1e60a8ba0e8306210ce4f63b19
anonymous Del Barco <61146153+maridasi47500@users.noreply.github.com>
2026-08-21 13:02:48 +0200

    qsf

--- hellopython.sh
+++ hellopython.sh
@@ -226,3 +226,7 @@ cp ~/list-repo/demofile.sh "/home/$USER/$1"
 (cd /home/$USER/$1 && git add .)
 (cd /home/$USER/$1 && git commit -am 'wow dljfghsfj')
 (cd /home/$USER/$1 && git push origin main)
+
+(cd /home/$USER/$1 && rm ~/list-repo/awesomemap.js)
+(cd /home/$USER/$1 && rm ~/list-repo/hellopython.sh)
+(cd /home/$USER/$1 && rm ~/list-repo/demofile.sh)

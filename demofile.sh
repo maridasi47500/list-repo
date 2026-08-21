2fd4e2faf6bc83568a930eadac70bdb9c44c0ac2
anonymous Del Barco <61146153+maridasi47500@users.noreply.github.com>
2026-08-21 10:29:57 +0200

    dfg

--- demofile.sh
+++ demofile.sh
@@ -6,4 +6,4 @@ python3 scaffold.py seasonal_sport name season
 python3 scaffold.py place_visit gem_quest_id:references user_id:references seasonal_sport_id:references
 python3 scaffold.py panomaric_view description gem_quest_id:references
 python3 scaffold.py job_offer name description user_id:references
-python3 scaffold.py myscore mymusic:staff pic user_id:references time_signature key_signature time_signature
+python3 scaffold.py myscore mymusic:staff pic user_id:references time_signature key_signature

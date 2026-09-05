a12889f36f3b44f05288b689053cceb42b37a50b
pirate du bato <maridasi@user.noresponse47500->
2026-09-05 23:57:45 +0200

    sdfg

--- demofile.sh
+++ demofile.sh
@@ -1,3 +1,6 @@
 
 mkdir templates 
-python3 scaffold.py mygpsdate lat lon description
+python3 scaffold.py country name
+python3 scaffold.py city country_id:references name
+python3 scaffold.py fiddle_type name
+python3 scaffold.py city_travel city_id:references fiddle_type_id:references title composer artist score:staff time_signature key_signature pic
